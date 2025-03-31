86a9ce2c1d96214f0ee0bfd185d0269d861f2f85
trianandafajar <mail.trianandafajar@gmail.com>
2025-03-31 23:44:15 +0700

    Update Navbar Component

--- src/components/NavbarComponents.jsx
+++ src/components/NavbarComponents.jsx
@@ -1,31 +1,35 @@
 import { Container, Image, Nav, NavDropdown, Navbar } from "react-bootstrap";
 
 const NavbarComponents = () => {
-  const avatar = (
-    <Image
-      src={"/img/img_avatar.png"}
-      alt="User"
-      roundedCircle
-      style={{ width: "30px" }}
-    />
-  );
   return (
-    <Navbar className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid>
         <Navbar.Brand href="#home">POS APP</Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav" className="justify-content-end">
           <Nav>
             <NavDropdown
               title={
                 <>
-                  {avatar} {"Pojok Code"}
+                  <Image
+                    src="/img/img_avatar.png"
+                    alt="User Avatar"
+                    roundedCircle
+                    width={30}
+                    className="me-2"
+                  />
+                  Pojok Code
                 </>
               }
               id="collapsible-nav-dropdown"
+              align="end"
             >
-              <NavDropdown.Item href="#">Profil</NavDropdown.Item>
-              <NavDropdown.Item href="/logout">Logout</NavDropdown.Item>
+              <NavDropdown.Item eventKey="profile" href="#">
+                Profil
+              </NavDropdown.Item>
+              <NavDropdown.Item eventKey="logout" href="/logout">
+                Logout
+              </NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
