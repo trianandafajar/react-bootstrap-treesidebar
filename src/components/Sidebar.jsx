fbda2bfe41e73033552d1e60c4ca45f6d42af561
trianandafajar <mail.trianandafajar@gmail.com>
2025-05-14 14:28:40 +0700

    Update Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,11 @@
-import { useState } from "react";
 import SidebarItem from "./SidebarItem.jsx";
 import { db } from "./db.js";
 
 const Sidebar = () => {
-  const [items] = useState(db); // Langsung inisialisasi tanpa useEffect
-
   return (
     <div className="sidebar shadow-sm bg-body-tertiary">
-      {items.map((item) => (
-        <SidebarItem key={item.id || item.title} item={item} />
+      {db.map((item) => (
+        <SidebarItem key={item.id} item={item} />
       ))}
     </div>
   );
