489d30405940ee7a1b0fc6fbbdb50d38a05e9411
trianandafajar <mail.trianandafajar@gmail.com>
2025-05-14 14:28:49 +0700

    Update Sidebar Item

--- src/components/SidebarItem.jsx
+++ src/components/SidebarItem.jsx
@@ -2,39 +2,30 @@ import { useState } from "react";
 import PropTypes from "prop-types";
 
 const SidebarItem = ({ item }) => {
+  const hasChildren = Array.isArray(item.childrens) && item.childrens.length > 0;
   const [isOpen, setIsOpen] = useState(item.isOpen === 1);
 
-  const toggleOpen = () => {
-    setIsOpen((prev) => !prev);
-  };
+  const toggleOpen = () => setIsOpen((prev) => !prev);
 
-  if (item.childrens) {
-    return (
-      <div className={`sidebar-item ${isOpen ? "open" : ""}`}>
-        <div className="sidebar-title" onClick={toggleOpen}>
-          <span>
-            {item.icon && <i className={item.icon}></i>}
-            {item.title}
-          </span>
-          <i className={`bi-chevron-left toggle-btn icon-item ${isOpen ? "rotated" : ""}`}></i>
-        </div>
-        {isOpen && (
-          <div className="sidebar-content">
-            {item.childrens.map((child) => (
-              <SidebarItem key={child.id || child.title} item={child} />
-            ))}
-          </div>
-        )}
+  return hasChildren ? (
+    <div className={`sidebar-item ${isOpen ? "open" : ""}`}>
+      <div className="sidebar-title" onClick={toggleOpen}>
+        <span>
+          {item.icon && <i className={item.icon}></i>} {item.title}
+        </span>
+        <i className={`bi-chevron-left toggle-btn icon-item ${isOpen ? "rotated" : ""}`}></i>
       </div>
-    );
-  } else {
-    return (
-      <a href={item.path || "#"} className="sidebar-item plain">
-        {item.icon && <i className={item.icon}></i>}
-        {item.title}
-      </a>
-    );
-  }
+      <div className={`sidebar-content ${!isOpen ? "d-none" : ""}`}>
+        {item.childrens.map((child) => (
+          <SidebarItem key={child.id} item={child} />
+        ))}
+      </div>
+    </div>
+  ) : (
+    <a href={item.path || "#"} className="sidebar-item plain">
+      {item.icon && <i className={item.icon}></i>} {item.title}
+    </a>
+  );
 };
 
 SidebarItem.propTypes = {
